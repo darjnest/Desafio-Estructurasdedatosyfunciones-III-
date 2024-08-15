d8fd9c3442fb16bd2499ee9f0f687c918a73446a
Carlos Silva Diaz <crasd15@live.com>
2024-08-14 21:10:31 -0400

    menu

---
+++ pizzza_jat/menu.py
@@ -0,0 +1,9 @@
+def mostrar_menu():
+    print("\n---- Bienvenido a Pizza JAT ----")
+    print("1. Seleccionar Masa")
+    print("2. Seleccionar Salsa")
+    print("3. Modificar Ingredientes")
+    print("4. Mostrar Ingredientes Actuales")
+    print("5. Estimar Tiempo y Confirmar Orden")
+    print("6. Salir")
+    return int(input("Seleccione una opción: "))
